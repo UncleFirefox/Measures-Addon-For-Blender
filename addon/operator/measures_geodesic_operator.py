e3a9eefc08acb189f78a77201a54c7eec6b31a6a
Albert Rodriguez <albertrfranco@gmail.com>
2021-02-16 21:50:48 +0100

    Porting geodesic code over part 2

--- addon/operator/measures_geodesic_operator.py
+++ addon/operator/measures_geodesic_operator.py
@@ -1,14 +1,6 @@
-from bmesh.types import BMesh
+from .geopath_datastructure import GeoPath
 import bpy
-import bmesh
 import traceback
-import math
-
-from mathutils import Euler
-from ..utility.draw import draw_quad, draw_text, get_blf_text_dims
-from ..utility.addon import get_prefs
-from ..utility.ray import mouse_raycast_to_scene
-from functools import reduce
 
 
 class MEASURES_GEODESIC_OT(bpy.types.Operator):
@@ -16,25 +8,24 @@ class MEASURES_GEODESIC_OT(bpy.types.Operator):
     bl_idname = 'measures.create_geodesic'
     bl_options = {"REGISTER", "UNDO", "BLOCKING"}
 
-    height: bpy.props.FloatProperty(name="Height", default=0, min=0)
-    plane_scale: bpy.props.FloatProperty(name="Plane Scale", default=1, min=1)
-    plane_rotation: bpy.props.FloatVectorProperty(
-        name="Plane Rotation",
-        subtype='EULER',
-        min=0, max=2*math.pi
-    )
-
     @classmethod
     def poll(cls, context):
         # If you want to verify the conditions of your operator
         # before it launches, put your code here
+        if context.mode != 'OBJECT':
+            return False
+
+        if context.object.type != 'MESH':
+            return False
+
         return True
 
     # Called after poll
     def invoke(self, context, event):
         # Initialize some props
-        self.hit_point = None
-        self.total_length = 0
+        self.geopath = GeoPath(context, context.object)
+        self.state = 'main'
+
         # Do some setup
         self.draw_handle = bpy.types.SpaceView3D.draw_handler_add(
             self.safe_draw_shader_2d, (context,), 'WINDOW', 'POST_PIXEL')
@@ -44,12 +35,34 @@ class MEASURES_GEODESIC_OT(bpy.types.Operator):
 
     # Running in loop until we leave the modal
     def modal(self, context, event):
+        if self.state == 'main':
+            return self.handle_main(self, context, event)
+        elif self.state == 'grab':
+            return self.handle_grab(self, context, event)
+
+        return {"RUNNING_MODAL"}  # Should not get here but you never know
+
+    # Handles events in main mode
+    def handle_main(self, context, event):
         # Free navigation
         if event.type == 'MIDDLEMOUSE':
             return {'PASS_THROUGH'}
 
-        # Confirm
+        # Grab initiating
+        elif event.type == 'G' and event.value == 'PRESS':
+            if self.geopath.grab_initiate():
+                self.state = 'grab'  # Do grab mode
+
+        # Adding points
         elif event.type == 'LEFTMOUSE' and event.value == 'PRESS':
+            x, y = event['mouse']
+            if self.geopath.seed is not None:
+                self.geopath.click_add_target(context, x, y)
+            else:
+                self.geopath.click_add_seed(context, x, y)
+
+        # Confirm path an exit gracefully
+        elif event.type == 'RET' and event.value == 'PRESS':
             self.remove_shaders(context)
             return {'FINISHED'}
 
@@ -58,16 +71,23 @@ class MEASURES_GEODESIC_OT(bpy.types.Operator):
             self.remove_shaders(context)
             return {'CANCELLED'}
 
-        # Adjust
-        elif event.type == 'MOUSEMOVE':
+        context.area.tag_redraw()
+        return {'RUNNING_MODAL'}
 
-            hit, location, normal, index, object, matrix = \
-                mouse_raycast_to_scene(context, event)
-
-            if hit:
-                self.height = location.z
-                self.hit_point = location
-                self.execute(context)
+    def handle_grab(self, context, event):
+        if event.type == 'LEFTMOUSE' and event.value == 'PRESS':
+            # confirm location
+            self.geopath.grab_confirm()
+            self.state = 'main'
+        elif event.type in {'RIGHTMOUSE', 'ESC'} and event.value == 'PRESS':
+            # put it back!
+            self.geopath.grab_cancel()
+            self.state = 'main'
+        if event.type == 'LEFTMOUSE':
+            # update the b_pt location
+            x, y = event['mouse']
+            self.geopath.grab_mouse_move(context, x, y)
+            self.state = 'grab'
 
         context.area.tag_redraw()
         return {'RUNNING_MODAL'}
@@ -75,117 +95,13 @@ class MEASURES_GEODESIC_OT(bpy.types.Operator):
     def draw(self, context):
         layout = self.layout
         layout.use_property_split = True
-        layout.prop(self, 'height')
-        layout.prop(self, 'plane_rotation')
+        # layout.prop(self, 'height')
+        # layout.prop(self, 'plane_rotation')
 
     def execute(self, context):
-        dg = context.evaluated_depsgraph_get()
-        scene = context.scene
-        ob = scene.objects.get("Avatar")
-        plane = self.create_plane()
-
-        if plane and ob:
-
-            # Get plane data
-            pmw = plane.matrix_world
-
-            if self.plane_rotation != Euler():
-                rotation_matrix = self.plane_rotation.to_matrix().to_4x4()
-                pmw = pmw @ rotation_matrix
-
-            face = plane.data.polygons[0]
-            plane_co = pmw @ face.center
-            plane_no = pmw @ (face.center + face.normal) - plane_co
-            bm = bmesh.new()
-            bm.from_object(ob, dg)
-            bmesh.ops.transform(
-                bm,
-                verts=bm.verts,
-                matrix=ob.matrix_world
-            )
-
-            # Perform bisection
-            bmesh.ops.bisect_plane(
-                bm,
-                geom=bm.faces[:] + bm.edges[:] + bm.verts[:],
-                clear_inner=True,
-                clear_outer=True,
-                plane_co=plane_co,
-                plane_no=plane_no
-            )
-
-            # Bisection cleanup
-            closest_edge = self.find_closest_edge(bm)
-
-            # Could be that the angle of plane finds no vertices after bisection
-            if closest_edge is not None:
-                vertex_list = self.get_reachable_vertices(closest_edge)
-                for v in [v for v in bm.verts if v not in vertex_list]:
-                    bm.verts.remove(v)
-
-                # Object creation and addition to scene
-                bisect_obj = bpy.data.objects.get("Bisect")
-                if bisect_obj is not None:
-                    bpy.data.objects.remove(bisect_obj, do_unlink=True)
-
-                me = bpy.data.meshes.new("Bisect")
-                bm.to_mesh(me)
-                ob = bpy.data.objects.new("Bisect", me)
-                context.collection.objects.link(ob)
-                ob.select_set(True)
-
-            bm.free()
-
+        # scene = context.scene
+        # ob = scene.objects.get("Avatar")
         return {'FINISHED'}
-
-    def get_reachable_vertices(self, closest_edge):
-        edge_list = [closest_edge]
-        vertex_list = [v for v in closest_edge.verts]
-        for v in vertex_list:
-            for e in [edge for edge in v.link_edges if edge not in edge_list]:
-                edge_list.append(e)
-                if (e.other_vert(v) not in vertex_list):
-                    vertex_list.append(e.other_vert(v))
-
-        self.total_length = reduce(lambda a, b: a + b.calc_length(), edge_list, 0)
-
-        return vertex_list
-
-    def find_closest_edge(self, bm: BMesh):
-        result = None
-        min_dist = 9999.99
-        for e in bm.edges:
-            for v in e.verts:
-                dist = (v.co - self.hit_point).magnitude
-                if dist < min_dist:
-                    result = e
-                    min_dist = dist
-                    if min_dist < 0.01:
-                        return e  # If its close enough dont continue iterating
-
-        return result
-
-    def create_plane(self):
-        mesh = bpy.data.meshes.new("Plane")
-        obj = bpy.data.objects.new("Plane", mesh)
-
-        # bpy.context.collection.objects.link(obj)
-
-        bm = bmesh.new()
-        bm.from_object(obj, bpy.context.view_layer.depsgraph)
-
-        s = self.plane_scale
-        bm.verts.new((s, s, self.height))
-        bm.verts.new((s, -s, self.height))
-        bm.verts.new((-s, s, self.height))
-        bm.verts.new((-s, -s, self.height))
-
-        bmesh.ops.contextual_create(bm, geom=bm.verts)
-
-        bm.to_mesh(mesh)
-        bm.free()
-
-        return obj
 
     def remove_shaders(self, context):
         '''Remove shader handle.'''
@@ -197,58 +113,9 @@ class MEASURES_GEODESIC_OT(bpy.types.Operator):
             context.area.tag_redraw()
 
     def safe_draw_shader_2d(self, context):
-
         try:
-            self.draw_shaders_2d(context)
+            self.geopath.draw(context)
         except Exception:
-            print("2D Shader Failed in Ray Caster")
+            print("Failed to draw geopath")
             traceback.print_exc()
             self.remove_shaders(context)
-
-    def draw_shaders_2d(self, context):
-
-        if (self.hit_point is None):
-            return
-
-        prefs = get_prefs()
-
-        # Props
-        text = "X : {:.3f}, Y : {:.3f}, Z : {:.3f}".format(
-            self.hit_point.x, self.hit_point.y, self.hit_point.z
-        )
-
-        font_size = prefs.settings.font_size
-        dims = get_blf_text_dims(text, font_size)
-        area_width = context.area.width
-        padding = 8
-
-        over_all_width = dims[0] + padding * 2
-        over_all_height = dims[1] + padding * 2
-
-        left_offset = abs((area_width - over_all_width) * .5)
-        bottom_offset = 20
-
-        top_left = (left_offset                 , bottom_offset + over_all_height)
-        bot_left = (left_offset                 , bottom_offset)
-        top_right = (left_offset + over_all_width, bottom_offset + over_all_height)
-        bot_right = (left_offset + over_all_width, bottom_offset)
-
-        # Draw Quad
-        verts = [top_left, bot_left, top_right, bot_right]
-        draw_quad(vertices=verts, color=prefs.color.bg_color)
-
-        # Draw Text
-        x = left_offset + padding
-        y = bottom_offset + padding
-        draw_text(
-            text=text, x=x, y=y, size=font_size,
-            color=prefs.color.font_color
-        )
-
-        # Draw measurement length
-        if self.total_length != 0:
-            text = "LENGTH: {:.3f}".format(self.total_length)
-            draw_text(
-                text=text, x=x, y=y + over_all_height + padding, size=font_size,
-                color=prefs.color.font_color
-            )
--- addon/utility/draw.py
+++ addon/utility/draw.py
@@ -1,19 +1,24 @@
-import bpy, blf, gpu
-from bgl import *
+import bpy
+import blf
+import gpu
+import bgl
+
 from gpu_extras.batch import batch_for_shader
+from bpy_extras.view3d_utils import location_3d_to_region_2d
 
 
-def draw_quad(vertices=[], color=(1,1,1,1)):
+def draw_quad(vertices=[], color=(1, 1, 1, 1)):
     '''Vertices = Top Left, Bottom Left, Top Right, Bottom Right'''
 
     indices = [(0, 1, 2), (1, 2, 3)]
     shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
-    batch = batch_for_shader(shader, 'TRIS', {"pos": vertices}, indices=indices)
+    batch = batch_for_shader(
+        shader, 'TRIS', {"pos": vertices}, indices=indices)
     shader.bind()
     shader.uniform_float("color", color)
-    glEnable(GL_BLEND)
+    bgl.glEnable(bgl.GL_BLEND)
     batch.draw(shader)
-    glDisable(GL_BLEND)
+    bgl.glDisable(bgl.GL_BLEND)
 
     del shader
     del batch
@@ -35,3 +40,56 @@ def get_blf_text_dims(text, size):
     dpi = bpy.context.preferences.system.dpi
     blf.size(0, size, dpi)
     return blf.dimensions(0, str(text))
+
+
+def draw_polyline_from_3dpoints(context, points_3d, color, thickness,
+                                LINE_TYPE):
+    '''
+    a simple way to draw a line
+    slow...becuase it must convert to screen every time
+    but allows you to pan and zoom around
+
+    args:
+        points_3d: a list of tuples representing x,y SCREEN coordinate
+        eg [(10,30),(11,31),...]
+        color: tuple (r,g,b,a)
+        thickness: integer? maybe a float
+        LINE_TYPE:  eg...bgl.GL_LINE_STIPPLE or
+    '''
+
+    points = [
+        location_3d_to_region_2d(context.region,
+                                 context.space_data.region_3d, loc)
+        for loc in points_3d
+    ]
+
+    if LINE_TYPE == "GL_LINE_STIPPLE":
+        bgl.glLineStipple(4, 0x5555)  # play with this later
+        bgl.glEnable(bgl.GL_LINE_STIPPLE)
+    bgl.glEnable(bgl.GL_BLEND)
+
+    bgl.glColor4f(*color)
+    bgl.glLineWidth(thickness)
+    bgl.glBegin(bgl.GL_LINE_STRIP)
+    for coord in points:
+        if coord:
+            bgl.glVertex2f(*coord)
+
+    bgl.glEnd()
+
+    if LINE_TYPE == "GL_LINE_STIPPLE":
+        bgl.glDisable(bgl.GL_LINE_STIPPLE)
+        bgl.glEnable(bgl.GL_BLEND)  # back to uninterupted lines
+        bgl.glLineWidth(1)
+    return
+
+
+def draw3d_points(context, points, color, size):
+    bgl.glColor4f(*color)
+    bgl.glPointSize(size)
+    bgl.glDepthRange(0.0, 0.997)
+    bgl.glBegin(bgl.GL_POINTS)
+    for coord in points:
+        bgl.glVertex3f(*coord)
+    bgl.glEnd()
+    bgl.glPointSize(1.0)
